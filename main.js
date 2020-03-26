397052d2456ba2541c39180a9711250d3f59cdae
mizanmahi <mizanmahi24@gmail.com>
2020-03-26 21:06:53 +0600

    click event handler adding

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ function playDrum (e){
     }
 }
 
+let keys = document.querySelectorAll(".keys__key");
+for(key of keys){
+    key.addEventListener("click", function(e){
+        let charcode = e.target.innerHTML.charCodeAt(0);
+        let aud = document.querySelector(`audio[data-key = "${charcode}"]`);
+        aud.currentTime = 0;
+        aud.play();
+        this.classList.add("playing");
+        
+        keys.forEach(key => {
+            key.addEventListener("transitionend", function(e){
+                if(e.propertyName !== "transform") return ;
+                this.classList.remove("playing")
+                
+            })
+        })
+        
+    })
+}
